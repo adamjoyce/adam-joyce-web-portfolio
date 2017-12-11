5a4ab6bb7d948c551127c24d728f700f3264d5a7
Adam Joyce <adamjoyce@outlook.com>
2017-12-11 18:22:14 +0000

    WIP - Desktop navigation refactored

--- static/js/base.js
+++ static/js/base.js
@@ -51,18 +51,18 @@ TypeWriter.prototype.tick = function() {
 };
 /* End of TypeWriter. */
 
-// /* Overlay navigation functionality for small screens. */
-// // Open overlay.
-// var overlay = document.getElementById('nav-overlay');
-// document.getElementById('nav-icon').addEventListener('click', function(e) {
-//   overlay.style.height = '100%';
-// });
-//
-// // Close Overlay.
-// document.getElementById('close-overlay').addEventListener('click', function(e) {
-//   overlay.style.height = '0%';
-// });
-//
+/* Overlay navigation functionality for small screens. */
+// Open overlay.
+var overlay = document.getElementById('nav-overlay');
+document.getElementById('nav-icon').addEventListener('click', function(e) {
+  overlay.style.height = '100%';
+});
+
+// Close Overlay.
+document.getElementById('close-overlay').addEventListener('click', function(e) {
+  overlay.style.height = '0%';
+});
+
 // // Page fade out before loading new page.
 // var pageLinks = document.getElementsByClassName('page-swap');
 // for (var i = 0; i < pageLinks.length; ++i) {
