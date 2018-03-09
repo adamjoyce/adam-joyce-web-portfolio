97014fe913a576cc81f3cfad76e0d95c83d6e4ee
Adam Joyce <adamjoyce@outlook.com>
2018-03-09 11:44:25 +0000

    Filters section expanding and collapsing smoothly for smaller screens

--- static/js/filters.js
+++ static/js/filters.js
@@ -9,13 +9,12 @@ var Filters = {
     var filters = document.getElementById('collapsible-filters');
     document.getElementById('toggle-filters').addEventListener(
       'click', function(e) {
-      if (filters.clientHeight == 0) {
-        filters.style.height = '100%';
-        console.log('MOVING TO 100%');
+      if (filters.clientHeight) {
+        filters.style.height = 0;
       }
       else {
-        filters.style.height = '0%';
-        console.log('MOVING TO 0%');
+        var measuringWrapper = document.getElementById('measuring-wrapper');
+        filters.style.height = measuringWrapper.clientHeight + 'px';
       }
     });
   }
