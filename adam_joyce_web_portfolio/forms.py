04a0ca288cc2a0e1a0ac3c761be50b6d0669e977
Adam Joyce <adamjoyce@outlook.com>
2018-04-18 16:55:17 +0100

    Form touch up and default image replacement

--- adam_joyce_web_portfolio/forms.py
+++ adam_joyce_web_portfolio/forms.py
@@ -3,16 +3,19 @@ from django import forms
 class EmailForm(forms.Form):
     first_name = forms.CharField(
         label='First Name',
+        label_suffix=' *',
         max_length=100,
         widget=forms.TextInput(attrs={'placeholder': 'Your first name'})
     )
     last_name = forms.CharField(
         label='Last Name',
+        label_suffix=' *',
         max_length=100,
         widget=forms.TextInput(attrs={'placeholder': 'Your last name'})
     )
     company_name = forms.CharField(
         label='Company Name',
+        label_suffix='',
         max_length=100,
         required=False,
         widget=forms.TextInput(
@@ -21,17 +24,20 @@ class EmailForm(forms.Form):
     )
     budget = forms.DecimalField(
         label='Budget',
+        label_suffix=' *',
         min_value=0,
         decimal_places=2,
         widget=forms.TextInput(attrs={'placeholder': 'Your budget in £\'s'})
     )
     email_address = forms.EmailField(
         label='Email',
+        label_suffix=' *',
         max_length=100,
         widget=forms.TextInput(attrs={'placeholder': 'Your email address'})
     )
     phone_number = forms.RegexField(
         label='Phone Number',
+        label_suffix=' *',
         regex='^\+\d{8,15}$',
         max_length=16,
         error_messages=
@@ -42,6 +48,7 @@ class EmailForm(forms.Form):
     )
     project_description = forms.CharField(
         label='Project Descrition',
+        label_suffix=' *',
         widget=forms.Textarea(
             attrs={'placeholder': 'A short description of your project...'}
         )
